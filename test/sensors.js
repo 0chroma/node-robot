30a16da160c8339b526425679c454c31596a7416
0chroma <21014+0chroma@users.noreply.github.com>
2014-06-20 16:54:38 -0400

    Added some stuff to adapter that manages loading the program onto the ev3 brick so we can get info and stuff

--- test/sensors.js
+++ test/sensors.js
@@ -1,8 +1,10 @@
 var sensors = require("../lib/sensors");
+var Adapter = require("../lib/ev3-adapter");
+
 
 suite("sensors", function(){
   test("should get touch sensor data", function(done){
-    var a = new Adapter(process.env.EV3_SERIAL_PORT);
+    var a = new Adapter(process.env.SERIAL_PORT);
     var s = sensors.TouchSensor(a, 1);
     s.read(function(err, val){
       console.log(val);
@@ -11,7 +13,7 @@ suite("sensors", function(){
   })
 
   test("should get color sensor data", function(done){
-    var a = new Adapter(process.env.EV3_SERIAL_PORT);
+    var a = new Adapter(process.env.SERIAL_PORT);
     var s = sensors.ColorSensor(a, 2);
     s.read(function(err, val){
       console.log(val);
@@ -20,7 +22,7 @@ suite("sensors", function(){
   })
 
   test("should get IR sensor data", function(done){
-    var a = new Adapter(process.env.EV3_SERIAL_PORT);
+    var a = new Adapter(process.env.SERIAL_PORT);
     var s = sensors.InfraSensor(a, 3);
     s.read(function(err, val){
       console.log(val);
