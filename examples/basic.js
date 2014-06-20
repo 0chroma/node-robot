4a0d79a7ccf73fd6a5faa3da3dffa4ebd339db53
0chroma <21014+0chroma@users.noreply.github.com>
2014-06-20 12:16:17 -0400

    initial commit, still getting things working

---
+++ examples/basic.js
@@ -0,0 +1,60 @@
+var MyRobot = function(){
+  Robot.apply(this, arguments);
+
+  this.on("")
+}
+
+util.inherits(MyRobot, Robot);
+
+MyRobot.prototype.moveForward = function(){
+
+}
+
+
+
+//------------------
+
+
+var r = new Robot();
+
+r.addState("moveForward", 
+  [["forward", 2000]]);
+
+r.stateListener("moveForward", "colorChange", function(oldColor, newColor, currentState) {
+  if newcolor == black
+    r.changeState("moveBackward");
+
+})
+
+
+
+//-------------------------
+
+
+var r = new Robot(/* ... */);
+
+r.run(function(){
+  r.on("sensor", function(type, oldValue, newValue){
+    if(type != "color") return;
+
+    //do data prcoessing
+    if(/* out of field */){
+      r.commands.back(2000).then(function(){
+        r.repeatedSequence([
+          ["back", 100],
+          function check(break){
+            if(r.sensors.color.getValue() == "black"){
+              r.commands.turn(180);
+              break();
+
+            }
+          }
+        ])
+      });
+    }
+  })
+  r.sequence([
+    ["forward", 2000],
+    /* ... */
+  ])
+});
---
+++ test/scheduler.js
@@ -0,0 +1,10 @@
+var Scheduler = require("../lib/scheduler");
+
+suite("Scheduler", function(){
+  test("should fire after time", function(done){
+    var s = new Scheduler();
+    s.scheduleTimeout(function(){
+      done();
+    }, 100)
+  })
+});
