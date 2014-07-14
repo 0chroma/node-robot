685c304be24a977f1ee695bea7da2a3c27c870d0
0chroma <21014+0chroma@users.noreply.github.com>
2014-07-14 14:04:13 -0400

    Fixed bug where tests were trying to read sensor data before the adapter was ready, fixed bug involving the request counter not being formatted correctly, added motor support to the ev3 adapter

--- test/ev3-adapter.js
+++ test/ev3-adapter.js
@@ -4,9 +4,11 @@ var runIfSp = require("./util").runIfSp;
 suite("Adapter", function(){
   test("should get sensor data", runIfSp(function(done){
     var a = new Adapter(process.env.SERIAL_PORT);
-    a.readSensor(1 /*port 1*/, 0, 10/*touch sensor*/, function(err, data){
-      console.log("raw readsensor data:" + data);
-      done();
+    a.once("ready", function(){
+      a.readSensor(1 /*port 1*/, 0, 10/*touch sensor*/, function(err, data){
+        console.log("raw readsensor data:" + data);
+        done();
+      })
     })
   }))
 });
--- test/ev3-sensors.js
+++ test/ev3-sensors.js
@@ -7,28 +7,34 @@ suite("Ev3 sensors", function(){
 
   test("should get touch sensor data", runIfSp(function(done){
     var a = new Adapter(process.env.SERIAL_PORT);
-    var s = new sensors.TouchSensor(a, 1);
-    s.read(function(err, val){
-      console.log("touch value: "+val);
-      done();
-    })
+    a.once("ready", function(){
+      var s = new sensors.TouchSensor(a, 1);
+      s.read(function(err, val){
+        console.log("touch value: "+val);
+        done();
+      })
+    });
   }))
 
   test("should get color sensor data", runIfSp(function(done){
     var a = new Adapter(process.env.SERIAL_PORT);
-    var s = new sensors.ColorSensor(a, 2, sensors.ColorSensor.modes.RINTENSITY);
-    s.read(function(err, val){
-      console.log("color value: "+val);
-      done();
-    })
+    a.once("ready", function(){
+      var s = new sensors.ColorSensor(a, 2, sensors.ColorSensor.modes.RINTENSITY);
+      s.read(function(err, val){
+        console.log("color value: "+val);
+        done();
+      })
+    });
   }))
 
   test("should get IR sensor data", runIfSp(function(done){
     var a = new Adapter(process.env.SERIAL_PORT);
-    var s = new sensors.InfraSensor(a, 3);
-    s.read(function(err, val){
-      console.log("ir value: "+val);
-      done();
-    })
+    a.once("ready", function(){
+      var s = new sensors.InfraSensor(a, 3);
+      s.read(function(err, val){
+        console.log("ir value: "+val);
+        done();
+      })
+    });
   }))
 });
