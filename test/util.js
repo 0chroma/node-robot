b3a7f2c959918ff09db0f00ee00d78d8d7519c00
0chroma <21014+0chroma@users.noreply.github.com>
2014-07-16 12:59:57 -0400

    added mock adapters to test utils for future use

--- test/util.js
+++ test/util.js
@@ -1,5 +1,19 @@
 module.exports = {
   runIfSp: function(testFunc){
     return process.env.SERIAL_PORT ? testFunc : null;
+  },
+  mockSensorAdapter: function(cb){
+    return {
+      readSensor: function(a,b,c,done){
+        return cb(done);
+      }
+    }
+  },
+  mockMotorAdapter: function(cb){
+    return {
+      setMotors: function(a,b,c,d,done){
+        return cb(done);
+      }
+    }
   }
 }
