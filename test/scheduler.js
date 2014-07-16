eadee4efb750ef2a1775e9e77b3346850da9accd
0chroma <21014+0chroma@users.noreply.github.com>
2014-07-16 12:16:17 -0400

    rewrote scheduler so that it works with sequences instead of tasks/functions

--- test/scheduler.js
+++ test/scheduler.js
@@ -1,10 +1,28 @@
 var Scheduler = require("../lib/scheduler");
+var assert = require('assert');
 
 suite("Scheduler", function(){
-  test("should fire after time", function(done){
+  test("should run a sequence", function(done){
     var s = new Scheduler();
-    s.scheduleTimeout(function(){
+    s.sequence(function(){
       done();
-    }, 30);
+    }).schedule();
+  });
+
+  test("should run a sequence after a delay", function(done){
+    var s = new Scheduler(true);
+    var called = false;
+    s.sequence(function(){
+      
+    }).after(30, function(){
+      called = true;
+    }).schedule();
+
+    s.sequence(function(){
+      assert(called);
+      done();
+    }).schedule();
+
+    s.start();
   })
 });
