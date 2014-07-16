073655c5775d95b74fcf99bf6af24fd275e5c991
0chroma <21014+0chroma@users.noreply.github.com>
2014-07-16 15:45:42 -0400

    fixed problem where there was no way of getting a callback on a sequence interrupt

--- test/sequence.js
+++ test/sequence.js
@@ -9,31 +9,30 @@ suite("Sequence", function(){
       ran.first = true;
     }).after(10, function(){
       ran.second = true;
-    }).once("true == true", function(){
+    }).when("true == true", function(){
       if(ran.first && ran.second)
         done();
       else
         throw Error("first two steps didn't run!");
     }).run();
   });
-  test("interrupts once() correctly", function(done){
+  test("interrupts when() correctly", function(done){
     var s = new Sequence();
-    var ran = false;
-    s.once("false == true", function(){
-      ran = true;
-      //this way if this ever gets called by stray code
-      //we'll get a done() called twice error
-      done();
+    var interrupted = false;
+    s.when("false == true", function(){
+      throw new Error("this should never run");
     }).run();
 
-    s.on("completed", function(interrupted){
-      if(ran) throw new Error("condition ran!");
-      if(!interrupted) throw new Error("wasn't interrupted!");
-      done();
+    s.on("completed", function(i){
+      console.log(i);
+      interrupted = i;
     })
 
     setTimeout(function(){
-      s.interrupt()
+      s.interrupt(function(){
+        assert(interrupted);
+        done();
+      })
     }, 30);
   });
   test("interrupts after() correctly", function(done){
@@ -57,11 +56,11 @@ suite("Sequence", function(){
     }, 30);
   });
 
-  test("once() gets run once a condition is fufilled", function(done){
+  test("when() gets run when a condition is fufilled", function(done){
     var s = new Sequence();
     var ran = false;
     var cond = false;
-    s.once(function(){ return cond == true }, function(){
+    s.when(function(){ return cond == true }, function(){
       ran = true;
     }, 5).do(function(){
       assert(ran);
